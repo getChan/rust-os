4e25ac877d3118df3a62962c3998e5dd1350ae8e
warm-inf <warm.inf@kakaoenterprise.com>
2022-06-23 17:28:07 +0900

    impl panic_handler

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+#![no_std]
+
+use core::panic::PanicInfo;
+
+// 패닉 발생 시 해당 함수 호출
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> !{
+    loop {
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
 }
