46f3975e6aa9a13628e07cbe1af2f1dbf7d516d3
warm-inf <warm.inf@kakaoenterprise.com>
2022-06-23 18:03:10 +0900

    freestanding at bare metal

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
-#![no_std]
+#![no_std] // Rust 표준 라이브러리를 링크하지 않도록 합니다
+#![no_main] // Rust 언어에서 사용하는 실행 시작 지점 (main 함수)을 사용하지 않습니다
 
 use core::panic::PanicInfo;
 
 // 패닉 발생 시 해당 함수 호출
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> !{
-    loop {
-    }
+    loop {}
 }
 
-fn main() {
+#[no_mangle] // 이 함수의 이름을 mangle하지 않습니다
+pub extern "C" fn _start() -> ! {
+    // 링커는 기본적으로 '_start' 라는 이름을 가진 함수를 실행 시작 지점으로 삼기에,
+    // 이 함수는 실행 시작 지점이 됩니다
+    loop {}
 }
+
+// 빌드하려면 다음과 같이 베어메탈 환경을 목표로 크로스 컴파일
+// `cargo build --target thumbv7em-none-eabihf`
