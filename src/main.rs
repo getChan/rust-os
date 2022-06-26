1314c3ad711e2458fc3e5926629e9294c106c57f
warm-inf <warm.inf@kakaoenterprise.com>
2022-06-26 18:23:13 +0900

    kernel boot

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,23 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+static HELLO: &[u8] = b"Hello World!";
+
 #[no_mangle] // 이 함수의 이름을 mangle하지 않습니다
 pub extern "C" fn _start() -> ! {
     // 링커는 기본적으로 '_start' 라는 이름을 가진 함수를 실행 시작 지점으로 삼기에,
     // 이 함수는 실행 시작 지점이 됩니다
+    let vga_buffer = 0xb8000 as *mut u8;
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
+// 커널 빌드하고 부트로더와 링크
+// cargo bootimage
+// 에뮬레이터에서 커널 부팅하기
+// qemu-system-x86_64 -drive format=raw,file=target/x86_64-os/debug/bootimage-os.bin
