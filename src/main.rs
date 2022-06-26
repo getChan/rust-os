fa8cf1739b002f5b913f2a5bc68bb42a4367b975
warm-inf <warm.inf@kakaoenterprise.com>
2022-06-26 16:57:49 +0900

    impl panic_handler

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use core::panic::PanicInfo;
 
 // 패닉 발생 시 해당 함수 호출
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> !{
+fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
@@ -15,6 +15,3 @@ pub extern "C" fn _start() -> ! {
     // 이 함수는 실행 시작 지점이 됩니다
     loop {}
 }
-
-// 빌드하려면 다음과 같이 베어메탈 환경을 목표로 크로스 컴파일
-// `cargo build --target thumbv7em-none-eabihf`
