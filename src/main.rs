73f00882a442ca3384f6cd0ab03d086fad817422
warm-inf <warm.inf@kakaoenterprise.com>
2022-06-29 23:54:29 +0900

    impl vga-buffer

--- src/main.rs
+++ src/main.rs
@@ -11,19 +11,12 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-static HELLO: &[u8] = b"Hello World!";
 
 #[no_mangle] // 이 함수의 이름을 mangle하지 않습니다
 pub extern "C" fn _start() -> ! {
     // 링커는 기본적으로 '_start' 라는 이름을 가진 함수를 실행 시작 지점으로 삼기에,
     // 이 함수는 실행 시작 지점이 됩니다
-    let vga_buffer = 0xb8000 as *mut u8;
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    vga_buffer::print_something();
 
     loop {}
 }
