41ae531a0481356de6f5da183e3125fd091111d0
warm-inf <warm.inf@kakaoenterprise.com>
2022-07-05 22:30:08 +0900

    exit QEMU

--- src/main.rs
+++ src/main.rs
@@ -34,6 +34,8 @@ fn test_runner(tests: &[&dyn Fn()]) {
     for test in tests {
         test();
     }
+
+    exit_qemu(QemuExitCode::Success);
 }
 
 #[test_case]
@@ -41,4 +43,21 @@ fn trivial_assertion() {
     print!("trivial assertion... ");
     assert_eq!(1, 1);
     println!("[ok]");
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    // 임의의 코드를 정함. QEMU default exit code와 구별되도록 함.
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
 }
