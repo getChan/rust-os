42397b6bcb82d9cbcc6bcd967a3e719cad883992
warm-inf <warm.inf@kakaoenterprise.com>
2022-07-09 15:50:53 +0900

    hide QEMU, print by Testable Trait

--- src/main.rs
+++ src/main.rs
@@ -41,20 +41,17 @@ pub extern "C" fn _start() -> ! {
 }
 
 #[cfg(test)]
-fn test_runner(tests: &[&dyn Fn()]) {
+fn test_runner(tests: &[&dyn Testable]) {
     serial_println!("Running {} tests", tests.len());
     for test in tests {
-        test();
+        test.run();
     }
-
     exit_qemu(QemuExitCode::Success);
 }
 
 #[test_case]
 fn trivial_assertion() {
-    serial_print!("trivial assertion... ");
-    assert_eq!(0, 1);
-    serial_println!("[ok]");
+    assert_eq!(1, 1);
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -71,5 +68,18 @@ pub fn exit_qemu(exit_code: QemuExitCode) {
     unsafe {
         let mut port = Port::new(0xf4);
         port.write(exit_code as u32);
+    }
+}
+
+pub trait Testable {
+    fn run(&self) -> ();
+}
+
+impl<T> Testable for T where T: Fn() {
+    fn run(&self) -> () {
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        // any::type_name() - string description of every type. For functions, their name
+        self();
+        serial_println!("[ok]");
     }
 }
