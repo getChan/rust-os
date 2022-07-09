7f222ba50e384db9f4ce3f74ec02b01f65227fdc
warm-inf <warm.inf@kakaoenterprise.com>
2022-07-09 17:18:58 +0900

    integration test

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,12 @@
 #![no_std] // Rust 표준 라이브러리를 링크하지 않도록 합니다
 #![no_main] // Rust 언어에서 사용하는 실행 시작 지점 (main 함수)을 사용하지 않습니다
-
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(os::test_runner)]
 #![reexport_test_harness_main = "test_main"] // cargo test에서 생성하는 함수의 이름을 test_main으로 변경
 
 use core::panic::PanicInfo;
 
-mod vga_buffer;
-mod serial;
-
-// 패닉 발생 시 해당 함수 호출
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    println!("{}", _info);
-    loop {}
-}
-
-// panic handler in test mode
-#[cfg(test)]
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    serial_println!("[failed]\n");
-    serial_println!("Error: {}\n", _info);
-    exit_qemu(QemuExitCode::Failed);
-    loop {}
-}
-
+use os::println;
 
 #[no_mangle] // 이 함수의 이름을 mangle하지 않습니다
 pub extern "C" fn _start() -> ! {
@@ -40,46 +19,16 @@ pub extern "C" fn _start() -> ! {
     loop {}
 }
 
+// 패닉 발생 시 해당 함수 호출
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    println!("{}", _info);
+    loop {}
+}
+
 #[cfg(test)]
-fn test_runner(tests: &[&dyn Testable]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test.run();
-    }
-    exit_qemu(QemuExitCode::Success);
-}
-
-#[test_case]
-fn trivial_assertion() {
-    assert_eq!(1, 1);
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-pub enum QemuExitCode {
-    // 임의의 코드를 정함. QEMU default exit code와 구별되도록 함.
-    Success = 0x10,
-    Failed = 0x11,
-}
-
-pub fn exit_qemu(exit_code: QemuExitCode) {
-    use x86_64::instructions::port::Port;
-
-    unsafe {
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32);
-    }
-}
-
-pub trait Testable {
-    fn run(&self) -> ();
-}
-
-impl<T> Testable for T where T: Fn() {
-    fn run(&self) -> () {
-        serial_print!("{}...\t", core::any::type_name::<T>());
-        // any::type_name() - string description of every type. For functions, their name
-        self();
-        serial_println!("[ok]");
-    }
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    os::test_panic_handler(_info);
 }
